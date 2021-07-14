c7d8c1b03aa7679e1e5ceb47cd9b3068c3fcf046
diazflo <bdiazflo@everis.com>
2021-07-14 15:43:54 -0500

    actualizaciones

--- src/main/java/com/everis/currency/entity/BootCoin.java
+++ src/main/java/com/everis/currency/entity/BootCoin.java
@@ -1,12 +1,13 @@
 package com.everis.currency.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
-
+@Data
 @Document
 public class BootCoin {
     @Id
@@ -18,51 +19,4 @@ public class BootCoin {
     private Date createDate;
     private Date lastUpdateDate;
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPurchaseCoin() {
-        return purchaseCoin;
-    }
-
-    public void setPurchaseCoin(BigDecimal purchaseCoin) {
-        this.purchaseCoin = purchaseCoin;
-    }
-
-    public BigDecimal getSellCoin() {
-        return sellCoin;
-    }
-
-    public void setSellCoin(BigDecimal sellCoin) {
-        this.sellCoin = sellCoin;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Date getLastUpdateDate() {
-        return lastUpdateDate;
-    }
-
-    public void setLastUpdateDate(Date lastUpdateDate) {
-        this.lastUpdateDate = lastUpdateDate;
-    }
 }
